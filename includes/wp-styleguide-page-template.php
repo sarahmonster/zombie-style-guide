84bce95b9bad3d2f6f50956c4f945c0f15f43894
John Cionci <john@thinkoomph.com>
2016-03-01 13:43:37 -0500

    Remove extraneous markup

--- includes/wp-styleguide-page-template.php
+++ includes/wp-styleguide-page-template.php
@@ -11,7 +11,7 @@
 get_header(); ?>
 
 	<section id="primary" class="content-area">
-		<main id="main" class="site-main sal-page" role="main">
+		<main id="main" class="site-main" role="main">
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
